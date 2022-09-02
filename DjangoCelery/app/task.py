8a37b2c3bc3a546737ae5452aaf629f814d2ee93
Rohit Jadhav <rohitdjadhav789@gmail.com>
2022-09-02 15:23:43 +0530

    email password details

--- DjangoCelery/app/task.py
+++ DjangoCelery/app/task.py
@@ -8,7 +8,7 @@ def send_email():
         subject='Test Mail',  #mail subject
         body="Testing Celery for sending mail",  #mail body
         from_email=os.environ.get('EMAIL_HOST_USER'), #sender email
-        to=('rohit.jadhav@weagile.net',) #tuple of reciever's email ids
+        to=('xyz@abc.com',) #tuple of reciever's email ids
     )
     email.send()
     return {'msg':'Done'}
